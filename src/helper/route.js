53c5ca7eca6925ff6e14f96223321b17769aeeff
panchunxin <run2pcx@163.com>
2019-05-08 20:33:41 +0800

    [UPDATE]生成文件链接

--- src/helper/route.js
+++ src/helper/route.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
+const Handlebars = require('handlebars');
+const path = require('path');
 const promisify = require('util').promisify;
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const config = require('../config/defaultConfig');
 
+const tplpath = path.join(__dirname, '../template/dir.tpl');
+const source = fs.readFileSync(tplpath);
+const template = Handlebars.compile(source.toString());
 module.exports = async function (req, res, pathFile) {
   try {
     const stats = await stat(pathFile);
@@ -13,8 +19,15 @@ module.exports = async function (req, res, pathFile) {
     } else if (stats.isDirectory()) {
       const files = await readdir(pathFile);
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(files.join(','));
+      res.setHeader('Content-Type', 'text/html');
+      const dir = path.relative(config.root, pathFile);
+      const data = {
+        title: path.basename(pathFile),
+        dir: dir ? `/${dir}` : '',
+        files
+      };
+      // res.end(files.join(','));
+      res.end(template(data));
     }
   } catch (ex) {
     console.log(ex)
